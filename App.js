b99d02a9fcaf1b0b0bcf1c69d1bd2dfefad55b2c
kyrrahnork <kyrrahnork@gmail.com>
2018-11-18 12:40:37 -0800

    fixes nav

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import Start from'./Start'
 import CourseView from './CourseView';
 import Login from './Login';
 import TestView from './TestView';
-import FlatListDemo from './FlatListDemo';
 
 console.disableYellowBox = true;
 
@@ -14,7 +13,6 @@ const RootStack = createStackNavigator(
     Start: Start,
     CourseView: CourseView,
     TestView: TestView,
-    FlatListDemo: FlatListDemo,
 
   },
   {
